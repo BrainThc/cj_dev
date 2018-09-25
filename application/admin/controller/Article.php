c6611b91b7b3a4cc05bdcd7438fcb5b60bf6cbcf
cj <tbrian@foxmail.com>
2018-09-25 17:54:31 +0800

    完成部分文章管理api

--- application/admin/controller/Article.php
+++ application/admin/controller/Article.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\admin\controller;
 use think\Controller;
+use app\admin\model\Article as ArticleModel;
+use app\admin\model\Log as LogModel;
 use think\Db;
 use think\Exception;
 
@@ -60,6 +62,55 @@ class Article extends Controller
 
     //添加文章提交
     public function create_article(){
+        $data = input('.post');
+        try{
+            //文章名
+            $insertData['title'] = empty($data['title']) ? '' : trim($data['title']);
+            if( $insertData == '' )
+                throw new Exception('文章名不能为空');
+
+            //关键词
+            $insertData['keyword'] = empty($data['keyword']) ? '' : $data['keyword'];
+            //描述
+            $insertData['description'] = empty($data['description']) ? '' : $data['description'];
+            $insertData['content'] = empty($data['content']) ? '' : $data['content'];
+
+            $t = time();
+            $insertData['add_time'] = $t;
+            $insertData['edit_time'] = $t;
+
+            $insertData['sequence'] = empty($insertData) ? 0 : intval($insertData['sequence']);
+            $insertData['read_group'] = ArticleModel::$map_raeadgroup[$data['read_group']];
+
+            Db::startTrans();
+            if( Db::table($this->articleModel->getTable())->insertData($insertData) === false )
+                throw new Exception('网络错误，添加失败');
+
+            //记录日志
+            $logModel = model('Log');
+            if( $logModel->note(LogModel::INSERT,'添加文章：'.$insertData['title']) === false )
+                throw new Exception('网络错误，操作失败');
+
+            Db::commit();
+        }catch( Exception $e ){
+            Db::rollback();
+            retrunJson(false,$e->getMessage());
+        }
+        returnJson(true,'添加成功');
+    }
+
+    /**
+     *  设置文章推荐
+     */
+    public function set_recomm(){
+        returnJson(false,'参数错误');
+    }
+
+    /*
+     *  设置文章状态
+     */
+    public function set_status(){
+        returnJson(false,'参数错误');
     }
 
     //编辑页面
--- application/admin/model/Article.php
+++ application/admin/model/Article.php
@@ -9,6 +9,20 @@ class Article extends Model{
 
     protected $name = 'article';
     protected $pk = 'article_id';
+
+    //阅读权限
+    const READ_ADD = 0;//全部可读
+    const READ_VIP = 1;//VIP用户
+    const READ_ADMIN = 2;//管理员
+
+    //阅读类型描述映射
+    public static $map_readgroup = array(
+        self::READ_ADD      => '所有用户',
+        self::READ_VIP      => '已注册用户',
+        self::READ_ADMIN    => '管理员'
+    );
+
+
 }
 
 ?>
