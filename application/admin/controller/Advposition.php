e7ed6bfeb3e3f3e02e8c0fb8d8fe854cebac4de0
BrianThc <tbrian@foxmail.com>
2018-10-10 15:01:15 +0800

    新添广告位管理模块控制器

---
+++ application/admin/controller/Advposition.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\admin\controller;
+use think\Db;
+use app\admin\model\Log as LogModel;
+use think\Exception;
+
+/**
+ * 广告位控制器
+ * Class Sysusergroup
+ * @package app\admin\controller
+ */
+class Advposition extends Base
+{
+
+}
