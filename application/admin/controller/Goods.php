3688b57c1462d0deda2f10be1196d6b1bb52f063
BrianThc <tbrian@foxmail.com>
2018-11-13 09:58:06 +0800

    提交商品sku配置代码内容

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -9,12 +9,73 @@ class Goods extends Base
     public function _initialize()
     {
         parent::_initialize();
-
+        $this->goodsModel = model('Goods');
     }
 
+    //列表主页
     public function index(){
-        echo '商品管理';
+        return $this->fetch();
     }
 
+    public function get_goods_list(){
+        $where = [];
+        $page_param = [];
+        $page = intval(input('page',1));;
+        $list = Db::table($this->goodsModel->getTable())->where($where)->paginate(15,false,['page'=>$page,'path'=>url('admin/goods/index',$page_param)]);
+        if( !empty($list) ){
+            foreach( $list as $key => $value ){
+                $value['oldSequence'] = $value['sequence'];
+                $list[$key] = $value;
+            }
+        }
+        $info['list'] = $list;
+        $pageHtml = $list->render();
+        $info['page'] = empty($pageHtml) ? '' : $pageHtml;
+        returnJson(true,'success',$info);
+    }
+
+    public function get_goods_cate(){
+        $cateModel = model('GoodsCategory');
+        $where = [];
+        $pid = intval(input('post.pid',0));
+        $where['pid'] = $pid;
+        $cate_list = Db::table($cateModel->getTable())->where($where)->select();
+        $info['cate_list'] = $cate_list;
+        returnJson(true,'success',$info);
+    }
+
+    //添加页面
+    public function add(){
+        return $this->fetch();
+    }
+
+    //获取分类属性选项卡
+    public function get_cate_prop_tab(){
+        $ids = intval(input('post.cate_id',0));
+        if( $ids <= 0 ){
+            returnJson(true,'success',[]);
+        }
+        $cateModel = model('GoodsCategory');
+        $catePropModel = model('GoodsCateProp');
+        $cate_all = $cateModel->getParentCate($ids);
+        $prop_list = $catePropModel->get_prop_list($cate_all,true);
+        if( !empty($prop_list) ){
+            foreach( $prop_list as $kprop => $prop ){
+                $prop['changed'] = false;
+                foreach( $prop['child'] as $ckey => $val ){
+                    $val['changed'] = false;
+                    $prop['child'][$ckey] = $val;
+                }
+                $prop_list[$kprop] = $prop;
+            }
+        }
+        $info = $prop_list;
+        returnJson(true,'success',$info);
+    }
+
+    //添加提交
+    public function create(){
+        returnJson(false);
+    }
 
 }
