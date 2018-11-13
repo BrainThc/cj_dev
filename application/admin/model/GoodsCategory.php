dd9acb13bc49d3ff7c0dd5739b9d9fe124ed5c71
BrianThc <tbrian@foxmail.com>
2018-11-13 09:54:27 +0800

    整合优化商品分类模型

--- application/admin/model/GoodsCategory.php
+++ application/admin/model/GoodsCategory.php
@@ -3,7 +3,7 @@ namespace app\admin\model;
 use think\Db;
 use think\Model;
 /**
- * 地区model
+ * 商品分类model
  * Class User
  */
 class GoodsCategory extends Model
@@ -40,6 +40,64 @@ class GoodsCategory extends Model
             }
         }
         return $cate_list;
+    }
+
+    /**
+     * 获取所有上级cate_id
+     * @param $cate_id          分类索引id
+     * @param $m                返回是否有自己id
+     * @return array            上级数组 含$cate_id
+     */
+    public function getParentCate($cate_id,$m=true){
+        $cate_id = intval($cate_id);
+        if( $cate_id <= 0 ){
+            return [];
+        }
+        $cate_array = [];
+        if($m){
+            $cate_array[] = $cate_id;
+        }
+        $pid = $cate_id;
+        do{
+           $info =  Db::table($this->getTable())
+                ->field('pid')
+                ->where('cate_id',$pid)
+                ->find();
+           if( empty($info) || $info['pid'] == 0 ){
+               return $cate_array;
+               break;
+           }else{
+               $cate_array[] = $info['pid'];
+               $pid = $info['pid'];
+           }
+        }while( $pid > 0 );
+        return $cate_array;
+    }
+
+    public function getChildCate($cate_id,$m=true){
+        $cate_id = intval($cate_id);
+        if( $cate_id <= 0 ){
+            return [];
+        }
+        $cate_array = [];
+        if($m){
+            $cate_array[] = $cate_id;
+        }
+        $pid = $cate_id;
+        do{
+            $info = Db::table($this->getTable())
+                ->field('cate_id')
+                ->where('pid',$pid)
+                ->find();
+            if( empty($info) ){
+                return $cate_array;
+                break;
+            }else{
+                $cate_array[] = $info['cate_id'];
+                $pid = $info['cate_id'];
+            }
+        }while( $pid > 0 );
+        return $cate_array;
     }
 
 }
